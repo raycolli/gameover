7b25172ceed6e47514ed9ab172209fef749158f2
raycolli <arrachelcollier@gmail.com>
2025-02-21 16:28:48 -0600

    changed text from sign in to login

--- pdf-quiz-app/pages/login.js
+++ pdf-quiz-app/pages/login.js
@@ -29,7 +29,7 @@ export default function Login() {
     <div className="min-h-screen bg-gray-900 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-blue-400">
-          Sign in to Note Nibbler
+          Login
         </h2>
       </div>
 
